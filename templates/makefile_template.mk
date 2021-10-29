238472fdc0d41138dd068b6d55dee1fd27a35df6
Paul Fry <paul-fry@hotmail.com>
2021-10-29 18:32:37 +1100

    housekeeping

--- templates/makefile_template.mk
+++ templates/makefile_template.mk
@@ -3,7 +3,7 @@ SHELL = /bin/sh
 # all: deps install [X, Y, Z...] clean
 
 eg_var ?=
-config_file := config.json
+config_file := envvars.json
 
 $(eval current_dir=$(shell pwd))
 $(eval program=$(shell jq '.Parameters.Program' ${config_file}))
@@ -13,6 +13,7 @@ installations: deps install clean
 .PHONY: deps
 deps:
 	$(info [+] Download the relevant dependencies)
+	pip install jq
 
 .PHONY: install
 install:
