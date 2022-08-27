82102a15e145fc43b0a7aa699c50c89172b202bf
Paul Fry <paul-fry@hotmail.com>
2022-08-27 11:18:38 +1000

    updated Makefile template

--- templates/makefile_template.mk
+++ templates/makefile_template.mk
@@ -13,16 +13,13 @@ $(eval program=$(shell jq '.Parameters.Program' ${CONFIG_FILE`}))
 
 installations: deps install clean
 
-.PHONY: deps
 deps:
 	$(info [+] Download the relevant dependencies)
 	pip install jq -q
 
-.PHONY: install
 install:
 	$(info [+] Install the relevant dependencies)
 
-.PHONY: clean
 clean:
 	$(info [+] Remove any redundant files, e.g. downloads)
 
