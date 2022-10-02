610c53300311f2f9702fe1a6f34f3ec2db4c97d9
Paul Fry <paul-fry@hotmail.com>
2022-10-02 22:09:23 +1100

    updated pre-commit template/files

---
+++ templates/pre_commit/ci/custom_markdown_rules.rb
@@ -0,0 +1,9 @@
+#===============================================================================
+# Start with all built-in rules.
+# https://github.com/markdownlint/markdownlint/blob/master/docs/RULES.md
+all
+
+#===============================================================================
+# Override default parameters for some built-in rules.
+# https://github.com/markdownlint/markdownlint/blob/master/docs/creating_styles.md#parameters
+exclude_rule 'MD013' # Max line length
