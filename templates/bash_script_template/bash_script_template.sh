3e869a13084d13a3de11ff638b43913f3b941f8c
Paul Fry <paul-fry@hotmail.com>
2025-04-02 14:09:20 +0100

    added set -e to bash script template

--- templates/bash_script_template/bash_script_template.sh
+++ templates/bash_script_template/bash_script_template.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Exit immediately if a command exits with a non-zero status (fail fast).
+set -e
+
 #=======================================================================
 # Variables
 #=======================================================================
